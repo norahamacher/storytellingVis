59fba940b4ad429440a9c60ca4cc519afadc343e
Nora <nora.hamacher@gmail.com>
2020-04-02 15:38:02 +1100

    Updates

---
+++ precache-manifest.30a81aef5f2e16cc0f3a470416d87d8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8594dfd8812fc6cbeaa9fe48a4edb61",
+    "url": "/storytellingVis/index.html"
+  },
+  {
+    "revision": "7fedf697214f698e3102",
+    "url": "/storytellingVis/static/css/main.1dab9a30.chunk.css"
+  },
+  {
+    "revision": "f8752e23d7edc2c09b50",
+    "url": "/storytellingVis/static/js/2.03ff4bb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/storytellingVis/static/js/2.03ff4bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fedf697214f698e3102",
+    "url": "/storytellingVis/static/js/main.f0e83e01.chunk.js"
+  },
+  {
+    "revision": "f7c34f11988f23f0e2f2",
+    "url": "/storytellingVis/static/js/runtime-main.703411f0.js"
+  },
+  {
+    "revision": "efb2d304e3844ed265f63d0c29553a1d",
+    "url": "/storytellingVis/static/media/storytellingvis.efb2d304.png"
+  },
+  {
+    "revision": "54cae726d0efe3e3f7f2a189a3a47ef3",
+    "url": "/storytellingVis/static/media/vispyramid.54cae726.png"
+  }
+]);
